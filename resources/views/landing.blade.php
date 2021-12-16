e09f14f0a57b9b0de6d7264d3a104890ea51239b
unknown <patrasioo69@gmail.com>
2021-12-16 09:55:51 +0100

    nav added

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -50,56 +50,61 @@
 <body>
     
   <header class="antialiased">
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+          <a href="https://getbootstrap.com/docs/5.1/examples/album/#" class="navbar-brand d-flex align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
+            <strong>Album</strong>
+          </a>
+          @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    {{-- <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a> --}}
-                    <a href="{{ route('home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">My events</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                    @endif
-                @endauth
+              @auth
+                {{-- <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a> --}}
+                <a href="{{ route('home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">My events</a>
+              @else
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+    
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                @endif
+              @endauth
             </div>
-        @endif
-        <div class="album py-5 bg-light">
-            <div class="container">
-              <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        
-                @foreach ($events as $itemevent)
-                    
-                <div class="col">
-                  <div class="card shadow-sm">
-                    {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> --}}
-                    <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="{{ $itemevent->image }}" alt="No funciona">
-                    <div class="card-body">
-                      <p class="card-text">{{ $itemevent->title }}</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                          <form action="/events" method="POST">
-                            @csrf
-                            @method('DELETE')
-                          </form>
-                          <button type="submit" class="btn btn-sm btn-outline-secondary">Delete</button> 
-                          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">10 - I Like</small>
-                      </div>
+          @endif
+        </div>
+      </div>
+      <div class="album py-5 bg-light">
+        <div class="container">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    
+            @foreach ($events as $itemevent)
+                
+            <div class="col">
+              <div class="card shadow-sm">
+                {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> --}}
+                <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="{{ $itemevent->image }}" alt="No funciona">
+                <div class="card-body">
+                  <p class="card-text">{{ $itemevent->title }}</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <form action="/events" method="POST">
+                        @csrf
+                        @method('DELETE')
+                      </form>
+                      <button type="submit" class="btn btn-sm btn-outline-secondary">Delete</button> 
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                     </div>
+                    <small class="text-muted">10 - I Like</small>
                   </div>
                 </div>
-        
-                @endforeach
-        
               </div>
             </div>
+    
+            @endforeach
+    
           </div>
-
-            </div>
         </div>
+      </div>
     </div>
   </header>
 
