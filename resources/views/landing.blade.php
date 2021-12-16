7fe1c0c743d4958ed13a28b5f8559444db1d36cd
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-12-16 12:09:25 +0100

    components

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<x-instantCard :instant = "$events[0]"/>
+{{-- <x-slider :events = '$events'/> --}}
+<x-test/>
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
