b8d0fc37222ce267eb9c092e7c66c918ef49c1e8
thisisariann <ari.menendez13@gmail.com>
2021-12-20 11:55:55 +0100

    logo on nav

--- resources/views/layouts/headHTML.blade.php
+++ resources/views/layouts/headHTML.blade.php
@@ -111,8 +111,7 @@
     <div class="navbar navbar-dark shadow-sm">
       <div class="container">
         <a href=" {{ route('landing') }}" class="navbar-brand d-flex align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
-          <strong>Album</strong>
+        <img src="../../../img/logo.png" style="width: 60%"/>  
         </a>
         <h1 style="font-size: 3em; color: black"><strong>The Meeting Point</strong></h1>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
