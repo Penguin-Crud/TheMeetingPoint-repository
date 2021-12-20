3980615187c2be8e2ac27bbe29cb1f1569ee82d7
thisisariann <ari.menendez13@gmail.com>
2021-12-20 09:30:15 +0100

    slider is working

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,6 +3,7 @@
   
 @section('main')
 <x-slider :events='$events'/>
+
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
