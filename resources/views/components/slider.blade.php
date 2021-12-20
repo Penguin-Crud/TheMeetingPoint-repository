cf7dd31cd3adac3149582b5b54355baeb5e735ad
thisisariann <ari.menendez13@gmail.com>
2021-12-20 10:30:18 +0100

    slider is working well

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -1,4 +1,4 @@
-{{--@props(['events'])
+@props(['events'])
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -8,35 +8,16 @@
     <div class="carousel-item">
       <img src="{{$events[$i]->image}}" class="d-block w-100">
     </div>
-  </div>
-  @endfor
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>--}}
-
-{{--<div id=“carouselExampleControls” class=“carousel slide” data-bs-ride=“carousel”>
-  <div class=“carousel-inner”>
-    <div class=“carousel-item active”>
-      <img src="{{$events[0]->image}}" class="d-block w-100">
-    </div>
-    @for ($i = 1; $i < count($events); $i++)
-      <div class=“carousel-item”>
-        <img src="{{$events[$i]->image}}" class="d-block w-100">
-      </div>
     @endfor
   </div>
-  <button class=“carousel-control-prev” type=“button” data-bs-target=“#carouselExampleControls” data-bs-slide=“prev”>
-    <span class=“carousel-control-prev-icon” aria-hidden=“true”></span>
-    <span class=“visually-hidden”>Previous</span>
-  </button>
-  <button class=“carousel-control-next” type=“button” data-bs-target=“#carouselExampleControls” data-bs-slide=“next”>
-    <span class=“carousel-control-next-icon” aria-hidden=“true”></span>
-    <span class=“visually-hidden”>Next</span>
-  </button>
-</div>--}}
+  <div>
+    <button class="carousel-control-prev" style="height:10%; top:50%" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" style="height:10%; top:50%" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,8 +2,9 @@
 
   
 @section('main')
-{{--<x-slider :events='$events'/>--}}
-
+    <div>
+     <x-slider :events='$events'/>
+    </div>
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
