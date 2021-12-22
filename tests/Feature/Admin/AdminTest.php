ec497674ac04f99fd0a9a29085fb7e6626a812e0
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-12-22 12:14:19 +0100

    Refactoring

--- tests/Feature/Admin/AdminTest.php
+++ tests/Feature/Admin/AdminTest.php
@@ -8,15 +8,16 @@ use Tests\TestCase;
 
 class AdminTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_admin_is_by_defaul_false()//unit test
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        
+        $this->assertTrue(true);
+        
     }
 }
