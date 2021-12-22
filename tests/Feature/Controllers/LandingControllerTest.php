b5132bf10697ea3766c6194ac8e48582d1fcc9c6
unknown <patrasioo69@gmail.com>
2021-12-22 13:40:33 +0100

    commit for git pull

--- tests/Feature/Controllers/LandingControllerTest.php
+++ tests/Feature/Controllers/LandingControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Events;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,11 +16,27 @@ class LandingControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_into_landing_page()
+
+    public function test_into_the_route_landing_page()
     {
+        User::factory()->create();
+        Events::factory(3)->create();
+
         $response = $this->get(route('landing'));
 
         $response->assertStatus(200)
         ->assertViewIs('landing');
+
+    }public function test_can_see_an_all_users_events_list()
+    {
+        $this->withoutExceptionHandling();
+
+        User::factory()->create();
+        Events::factory(2)->create();
+        $event = Events::all();
+
+        $response = $this->get(route('landing'));
+
+        $response->assertSee($event[2]->title);
     }
 }
