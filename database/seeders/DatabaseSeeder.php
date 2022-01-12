ed59309bb2607557db604cd1864ec78939f34930
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2022-01-12 12:35:31 +0100

    Seed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,29 +22,38 @@ class DatabaseSeeder extends Seeder
             'isAdmin' => true,
         ])->create();
 
+        $user = User::factory([
+            'email' => 'user@user.com',
+            'name' => 'user',
+            'isAdmin' => false,
+        ])->create();
+
         User::factory(2)->create();
 
-        Events::factory()->create([
+        $event1 = Events::factory()->create([
             'title' => 'guayaquil',
             'image' => 'https://pymstatic.com/85493/conversions/coach-guayaquil-default.jpg',
             'user_id' => '1',
             'showSlider' => true,
             'description' => 'Lorem Ipsum facty como que n tee juhy jauen fpaiaif e napamte lemfasmee e fajef  najdjw wkajwu wmd.',
         ]);
-        Events::factory()->create([
+        $event2 = Events::factory()->create([
             'title' => 'barcelona',
             'image' => 'https://media.cntraveler.com/photos/5a985924d41cc84048ce6db0/master/w_4348,h_3261,c_limit/Catedral-de-Barcelona-GettyImages-511874340.jpg',
             'user_id' => '1',
             'showSlider' => true,
         ]);
-        Events::factory()->create([
+        $event3 = Events::factory()->create([
             'title' => 'Parc Guell',
             'image' => 'https://lp-cms-production.imgix.net/2021-07/GettyRF_1137803766.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850',
             'user_id' => '1',
             'showSlider' => true,
         ]);
-        
-
         Events::factory(10)->create();
+
+        $event1->addStudent($user->id);
+        $event2->addStudent($user->id);
+        $event3->addStudent($user->id);
+        
     }
 }
