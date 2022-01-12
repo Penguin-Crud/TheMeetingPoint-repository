04506aeb2b3020f10ed1f9c16e2d518d361e64e6
timfel20 <timmyfelicia@gmail.com>
2022-01-12 12:37:39 +0100

    cambio de color de max people y la fecha

--- resources/views/components/eventCard.blade.php
+++ resources/views/components/eventCard.blade.php
@@ -2,7 +2,7 @@
 <div class="col">
     <div class="card shadow-sm">
       {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> --}}
-      <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="{{ $itemEvent->image }}" alt="No funciona">
+      <img class="bd-placeholder-img card-img-top" style="object-fit:cover" width="100%"  height="225" src="{{ $itemEvent->image }}" alt="No funciona">
       <div class="card-body">
         <div class="d-flex justify-content-between">
           <p class="card-text" style="font-weight:bold">{{ $itemEvent->title }}</p>
