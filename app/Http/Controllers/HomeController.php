dbb1507f4ed0ae990b5b69f75d4d5f4fce53ac46
Exodya21 <jaumegomiciai@gmail.com>
2022-01-13 14:42:53 +0100

    images factorys & homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Events;
+use Illuminate\Console\Scheduling\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
 
 class HomeController extends Controller
 {
@@ -27,14 +30,33 @@ class HomeController extends Controller
     {
         return view('home');
     }
-
+    
     public function allowEvent(Events $events) // lo verde identifica que el parametro encuentra relacion con un model existente con el mismo nombre
     {
-        // dd(auth()->user()->id);
-        dd($events);
-        // $eventToAllow = Events::findOrFail($id);
         $events->addStudent(auth()->user()->id);
 
-        return redirect('home');
+        $list = [];
+        $user_id = Auth()->user()->id;
+        
+        $allowEventsList = DB::table('students')->where('user_id', $user_id)->get();
+
+        foreach ($allowEventsList as $itemAllowEvent) {
+            $id = $itemAllowEvent->events_id;
+            $x = Events::where('id', $id)->get();
+            array_push($list, $x);
+        }
+        // dd($list[0][0]);
+        // $cosa = Events::all();
+        // dd($cosa[0]);
+        // dd($allowEventsList);
+
+
+        // dd(auth()->user());
+        // dd($events);
+        // $eventToAllow = Events::findOrFail($id);
+
+        return $list;
     }
+
+    
 }
